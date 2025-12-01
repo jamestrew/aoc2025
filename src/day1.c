4d8514bf78f7f767338ec2b3b07fdb62d0d6c313
James Trew <j.trew10@gmail.com>
2025-12-01 09:52:08 -0500

    day1, part1

---
+++ src/day1.c
@@ -0,0 +1,92 @@
+#include "util.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int DIAL_MIN = 0;
+const int DIAL_MAX = 99;
+const int DIAL_POSITIONS = 100;
+
+int *parse_instruction(const char *input, int *count) {
+  char **lines = split(input, "\n", count);
+
+  int *instructions = malloc(*count * sizeof(int));
+  assert(instructions);
+
+  for (int i = 0; i < *count; ++i) {
+    char *line = lines[i];
+
+    int dir;
+    if (line[0] == 'L')
+      dir = -1;
+    else if (line[0] == 'R')
+      dir = 1;
+    else {
+      fprintf(stderr, "expected L##/R## from line. got %s", line);
+      abort();
+    }
+
+    int clicks = strtol(line + 1, NULL, 10);
+    instructions[i] = dir * clicks;
+  }
+
+  free(lines);
+  return instructions;
+}
+
+int part1(const char *input) {
+  int instr_count = 0;
+  int *instructions = parse_instruction(input, &instr_count);
+
+  int curr_num = 50;
+  int zeroed_count = 0;
+
+  for (int i = 0; i < instr_count; ++i) {
+    int clicks = instructions[i];
+    curr_num = (curr_num + clicks) % DIAL_POSITIONS;
+    if (curr_num < DIAL_MIN)
+      curr_num += DIAL_POSITIONS;
+
+    if (curr_num == 0)
+      zeroed_count++;
+  }
+
+  free(instructions);
+  return zeroed_count;
+}
+
+int part2(const char *input) { return 0; }
+
+void test() {
+  const char *test_input = "L68\n"
+                           "L30\n"
+                           "R48\n"
+                           "L5\n"
+                           "R60\n"
+                           "L55\n"
+                           "L1\n"
+                           "L99\n"
+                           "R14\n"
+                           "L82";
+
+  assert(part1(test_input) == 3);
+  assert(part2(test_input) == 0);
+  printf("All tests passed!\n");
+}
+
+int main() {
+  printf("here we go...");
+  test();
+
+  char *input = read_day_input(1);
+  if (!input) {
+    fprintf(stderr, "Failed to read input\n");
+    return 1;
+  }
+
+  printf("Part 1: %d\n", part1(input));
+  printf("Part 2: %d\n", part2(input));
+
+  free(input);
+  return 0;
+}
