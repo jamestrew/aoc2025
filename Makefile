6b06d92299890b35e956526bac7cb1cb725a8da8
James Trew <j.trew10@gmail.com>
2025-12-05 13:18:16 -0500

    day5 part2

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -Wconversion -g -fPIC
+CFLAGS = -Wall -Wextra -Wconversion -fPIC
 RELEASE_CFLAGS = -Wall -Wextra -Wconversion -O2 -DNDEBUG -fPIC
+DEBUG_CFLAGS = -Wall -Wextra -Wconversion -fPIC -g -Og
 LDFLAGS = -ldl -lm
 SRC_DIR = src
 BUILD_DIR = build
@@ -25,6 +26,9 @@ aoc: $(BUILD_DIR)/main.o $(UTIL_OBJ) | $(BIN_DIR)
 
 day%: $(SRC_DIR)/day%.c $(UTIL_OBJ) | $(BIN_DIR)
 	$(CC) $(RELEASE_CFLAGS) $< $(UTIL_OBJ) -o $(BIN_DIR)/$@ -lm
+
+debug%: $(SRC_DIR)/day%.c $(UTIL_OBJ) | $(BIN_DIR)
+	$(CC) $(DEBUG_CFLAGS) $< $(UTIL_OBJ) -o $(BIN_DIR)/$@ -lm
 
 $(BUILD_DIR) $(BIN_DIR):
 	mkdir -p $@
--- src/day5.c
+++ src/day5.c
@@ -15,6 +15,13 @@ typedef struct {
   uint64_t end;
 } Range;
 
+void print_ranges(const Range *ranges, size_t rng_size) {
+  for (size_t i = 0; i < rng_size; ++i) {
+    Range r = ranges[i];
+    printf("%zu-%zu\n", r.start, r.end);
+  }
+}
+
 void parse_ranges(const char *input, Range **ranges, size_t *rng_size) {
   char **lines = split(input, "\n", rng_size);
   *ranges = realloc(*ranges, *rng_size * sizeof(Range));
@@ -25,6 +32,7 @@ void parse_ranges(const char *input, Range **ranges, size_t *rng_size) {
     assert(c == 2);
     (*ranges)[i].start = strtoull(parts[0], NULL, 10);
     (*ranges)[i].end = strtoull(parts[1], NULL, 10);
+    free_lines(parts, c);
   }
 
   free_lines(lines, *rng_size);
@@ -38,9 +46,48 @@ bool in_ranges(Range *ranges, size_t rng_size, uint64_t ing) {
   return false;
 }
 
+Range *coalesce_ranges(Range *ranges, size_t *rng_size) {
+  Range *new_rng = malloc(sizeof(Range) * *rng_size);
+  size_t new_size = 0;
+
+  for (size_t i = 0; i < *rng_size; ++i) {
+    Range test = ranges[i];
+    bool add = true;
+    for (size_t j = i + 1; j < *rng_size; ++j) {
+      Range *comp = &(ranges[j]);
+
+      if (test.start >= comp->start && test.start <= comp->end) {
+        if (test.end > comp->end) {
+          comp->end = test.end;
+        }
+        add = false;
+      } else if (test.end >= comp->start && test.end <= comp->end) {
+        if (test.start < comp->start) {
+          comp->start = test.start;
+        }
+        add = false;
+      } else if (test.start < comp->start && test.end > comp->end) {
+        comp->start = test.start;
+        comp->end = test.end;
+        add = false;
+      }
+    }
+
+    if (add) {
+      new_rng[new_size].start = test.start;
+      new_rng[new_size].end = test.end;
+      new_size++;
+    }
+  }
+
+  free(ranges);
+  *rng_size = new_size;
+  return new_rng;
+}
+
 void parse_ingredients(const char *input, uint64_t **ingredients, size_t *ing_count) {
   char **lines = split(input, "\n", ing_count);
-  *ingredients = realloc(*ingredients, *ing_count * sizeof(int));
+  *ingredients = realloc(*ingredients, *ing_count * sizeof(uint64_t));
 
   for (size_t i = 0; i < *ing_count; ++i) {
     (*ingredients)[i] = strtoull(lines[i], NULL, 10);
@@ -59,17 +106,12 @@ char **split_input_parts(const char *input) {
     }
   }
 
-  /* printf("ranges(%zu):\n%s\n", strlen(parts[0]), parts[0]); */
-  /* printf("ingredients(%zu):\n%s\n", strlen(parts[1]), parts[1]); */
   return parts;
 }
 
 void parse_input(const char *input, Range **ranges, size_t *rng_size, uint64_t **ingredients, size_t *ing_count) {
   char **parts = split_input_parts(input);
   parse_ranges(parts[0], ranges, rng_size);
-  printf("rangesp: %p\n", ranges);
-  printf("ranges(%zu):\n", *rng_size);
-
   parse_ingredients(parts[1], ingredients, ing_count);
   free_lines(parts, input_parts);
 }
@@ -83,11 +125,6 @@ uint64_t part1(const char *input) {
   size_t rng_size = 0;
   parse_input(input, &ranges, &rng_size, &ingredients, &ing_count);
 
-  printf("ingredients(%zu)\n", ing_count);
-  for (size_t i = 0; i < rng_size; ++i) {
-    printf("%zu", ingredients[i]);
-  }
-
   for (size_t i = 0; i < ing_count; ++i) {
     if (in_ranges(ranges, rng_size, ingredients[i]))
       fresh_count++;
@@ -98,7 +135,22 @@ uint64_t part1(const char *input) {
   return fresh_count;
 }
 
-uint64_t part2(const char *input) { return 0; }
+uint64_t part2(const char *input) {
+  char **parts = split_input_parts(input);
+  Range *ranges = NULL;
+  size_t rng_size = 0;
+  parse_ranges(parts[0], &ranges, &rng_size);
+  ranges = coalesce_ranges(ranges, &rng_size);
+
+  uint64_t fresh_count = 0;
+  for (size_t i = 0; i < rng_size; ++i) {
+    fresh_count += ranges[i].end - ranges[i].start + 1;
+  }
+
+  free_lines(parts, 2);
+  free(ranges);
+  return fresh_count;
+}
 
 void test() {
   const char *test_input =
@@ -116,11 +168,12 @@ void test() {
       "32";
 
   ASSERT_EQ(3, part1(test_input));
-  ASSERT_EQ(0, part2(test_input));
+  ASSERT_EQ(14, part2(test_input));
   printf("All tests passed!\n");
 }
 
 int main() {
+  test();
   char *input = read_day_input(DAY);
   printf("Part 1: %lu\n", part1(input));
   printf("Part 2: %lu\n", part2(input));
